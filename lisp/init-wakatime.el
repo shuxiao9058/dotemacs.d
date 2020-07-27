a0721de54fc753421a289a748f00fc1f37616294
Aaron Chi <shuxiao9058@gmail.com>
2020-07-27 23:17:54 +0800

    wakatime

--- lisp/init-wakatime.el
+++ lisp/init-wakatime.el
@@ -2,11 +2,13 @@
 
 (use-package wakatime-mode
   :straight t
-  :init (setq +wakatime-hide-filenames t)
+  :init
+  (setq +wakatime-hide-filenames t)
+  (when IS-MAC
+    (setq wakatime-cli-path "/usr/local/bin/wakatime"
+          wakatime-python-bin "/usr/local/anaconda3/bin/python3"))
   :hook ((org-mode . wakatime-mode)
          (prog-mode . wakatime-mode))
-  :custom ((wakatime-cli-path "/usr/local/bin/wakatime")
-           (wakatime-python-bin "/usr/local/anaconda3/bin/python3"))
   :config
   (global-wakatime-mode +1)
   )
