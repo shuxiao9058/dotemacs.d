ec19a570094431745ea204cafa312d3efded2f3e
Aaron Chi <shuxiao9058@gmail.com>
2021-04-04 01:23:51 +0800

    update meow setup

--- init.el
+++ init.el
@@ -88,12 +88,6 @@
 
 ;; (require 'init-misc)
 
-;; keybindings
-;; early load with general in core
-;; (eval-after-load 'general
-;;   `(progn
-;;      (require 'init-keybindings)))
-
 ;; Load customization file now
 (let ((custom-file (expand-file-name "custom.el" user-emacs-directory)))
   (when (file-exists-p custom-file)
