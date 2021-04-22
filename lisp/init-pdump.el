eed45815ac865ff9c34d150956d632e8940f840e
Aaron Chi <shuxiao9058@gmail.com>
2021-04-22 18:04:32 +0800

    update

--- lisp/init-pdump.el
+++ lisp/init-pdump.el
@@ -152,7 +152,11 @@ the end of the loading of the dump file."
                (setq dump/dump-process nil)))
            :command
            (list ;; "emacs"
-	    "/run/current-system/Applications/Emacs.app/Contents/MacOS/Emacs.sh"
+	    (if IS-MAC
+		(if IS-AARCH64
+		    "/opt/homebrew/opt/emacs-mac/Emacs.app/Contents/MacOS/Emacs.sh"
+		  "/run/current-system/Applications/Emacs.app/Contents/MacOS/Emacs.sh")
+	      "emacs")
             "--batch"
             "-l" (expand-file-name "pdump.el" user-emacs-directory)
             "-eval" (concat "(dump/dump-emacs-portable \"" dump-file-temp "\")"))))
